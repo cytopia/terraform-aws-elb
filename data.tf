a90a7a21e265e5e0cdb8cb6ac4b1fed99630ede9
Dan Vaida <me@danvaida.com>
2020-10-26 14:52:31 +0100

    Satisfies the linting rules of an older TF version

--- data.tf
+++ data.tf
@@ -1,9 +1,11 @@
 data "aws_security_groups" "elb" {
   count = "${length(var.security_group_names) >= 1 ? 1 : 0}"
+
   filter {
     name   = "group-name"
     values = ["${element(var.security_group_names, count.index)}"]
   }
+
   filter {
     name   = "vpc-id"
     values = ["${var.vpc_id}"]
